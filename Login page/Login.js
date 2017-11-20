8a1a1e8a86eb7202a7c8c9056bb6db380f2792cc
lingshenzhe <820683890@qq.com>
2017-11-20 23:44:41 +0800

    new js
    新改的js，上了登录转跳页面

--- Login page/Login.js
+++ Login page/Login.js
@@ -1,29 +1,35 @@
-$.ajax({
-    url:'http://123.206.18.85:8090/login',
-    type:'POST', 
-    async:true, 
-    data:{ 
-        "login_id":"admin",
-        "password":"admin"
-    },
-    timeout:5000,  
-    dataType:'json',   
-    beforeSend:function(xhr){
-        console.log(xhr)
-        console.log('发送前')
-    },
-    success:function(data,textStatus,jqXHR){
-        console.log(data)
-        console.log(textStatus)
-        console.log(jqXHR)
-    },
-    error:function(xhr,textStatus){
-        console.log('错误')
-        console.log(xhr)
-        console.log(textStatus)
-    },
-    complete:function(){
-        console.log('结束')
-    }
-})
-
+function login(){
+    $.ajax({
+        url: 'http://123.206.18.85:8090/login',
+        type: 'POST',
+        async: true,
+        data: {
+            "login_id": "admin",
+            "password": "admin"
+        },
+        timeout: 5000,
+        dataType: 'json',
+        beforeSend: function (xhr) {
+            console.log(xhr)
+            console.log('发送前')
+        },
+        success: function (data, textStatus, jqXHR) {
+            console.log(data)
+            console.log(textStatus)
+            console.log(jqXHR)
+            if (data.errorCode == 200) {
+                    window.location.href = "../main/main.html";
+            } else {
+                console.log(data.errorMsg)
+            }
+        },
+        error: function (xhr, textStatus) {
+            console.log('错误')
+            console.log(xhr)
+            console.log(textStatus)
+        },
+        complete: function () {
+            console.log('结束')
+        }
+    })
+}
