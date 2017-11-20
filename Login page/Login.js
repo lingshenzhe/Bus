1a115fac8c35e56abf952218318e803aa9b6a589
lingshenzhe <820683890@qq.com>
2017-11-20 22:59:25 +0800

    login
    bus login

---
+++ Login page/Login.js
@@ -0,0 +1,29 @@
+$.ajax({
+    url:'http://123.206.18.85:8090/login',
+    type:'POST', 
+    async:true, 
+    data:{ 
+        "login_id":"admin",
+        "password":"admin"
+    },
+    timeout:5000,  
+    dataType:'json',   
+    beforeSend:function(xhr){
+        console.log(xhr)
+        console.log('发送前')
+    },
+    success:function(data,textStatus,jqXHR){
+        console.log(data)
+        console.log(textStatus)
+        console.log(jqXHR)
+    },
+    error:function(xhr,textStatus){
+        console.log('错误')
+        console.log(xhr)
+        console.log(textStatus)
+    },
+    complete:function(){
+        console.log('结束')
+    }
+})
+
